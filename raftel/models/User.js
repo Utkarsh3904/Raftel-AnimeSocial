6457e054326eb3a29fa77407d2387173824c4c4d
Utkarsh3904 <millenniumatom@gmail.com>
2026-06-04 19:48:31 +0530

    fix: correct clerkId typo and restructure top5Anime schema

--- raftel/models/User.js
+++ raftel/models/User.js
@@ -10,14 +10,29 @@ const animeSchema = new mongoose.Schema({
 })
 
 const UserSchema = new mongoose.Schema({
-    clearkId: {type: String, required:true, unique: true},
+    clerkId: {type: String, required:true, unique: true},
     username: {type: String, required:true },
     avatar: {type: String, required:true},
-    watchedAnime: [animeSchema],
-    top5Anime: {type: [animeSchema], validate: v => v.length<=5},
-    reputation: {type: number, default :0},
+    top5Anime: {
+      type: [
+        {
+          id: Number,
+          title: {
+            romaji: String,
+          },
+          coverImage: {
+            medium: String,
+          },
+        },
+      ],
+      validate: {
+        validator: v => v.length <= 5,
+        message: 'top5Anime can only contain up to 5 items',
+      },
+    },
+    reputation: {type: Number, default :0},
     onBoard : {type: Boolean, default: false}
-})
+}, { timestamps: true })
 
     export default mongoose.models.User || mongoose.model('User', UserSchema)  
 
