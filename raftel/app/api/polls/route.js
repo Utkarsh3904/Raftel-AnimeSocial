0fb9699f31a9cc809944038a9e2f6d58eaf3a8e6
Utkarsh3904 <millenniumatom@gmail.com>
2026-06-05 22:58:48 +0530

    chore: add whitespace for improved readability in POST function

--- raftel/app/api/polls/route.js
+++ raftel/app/api/polls/route.js
@@ -19,6 +19,7 @@ export async function GET() {
 }
 
 export async function POST(req) {
+  
   const { userId } = auth()
   if (!userId) return Response.json({ error: "Unauthorized" }, { status: 401 })
 
